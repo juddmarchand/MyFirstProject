111001d27d9013cc451f93021282633103425c7a
juddmarchand <juddmarchand@gmail.com>
2023-09-12 08:38:46 -0500

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,12 @@
+/*
+Judd Marchand
+C++ 2023
+Lab 1 Introduction to C++ Programming
+This lab will serve as a demonstration of your ability to download a project from GitHub, compile and run the project, upload a project to GitHub, and submit a project to Canvas.
+*/
+#include <iostream>
+int main()
+{
+	std::cout << "Hello World!";
+	std::cout << "Blue";
+}
